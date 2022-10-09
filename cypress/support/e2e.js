e3196a800dd626babab3165cd9040256ed43985f
Filip Hric <filip@filiphric.com>
2022-10-09 20:48:38 +0200

    add cypress plugin api

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 import '../../workshop-scripts/testSetupHook'
 import '../../workshop-scripts/infoCommand'
-import '@bahmutov/cy-api'
+import 'cypress-plugin-api'
 chai.use(require('chai-json-schema'));
 
 Cypress.Commands.overwrite('request', (orig, ...args) => {
